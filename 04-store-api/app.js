8dad2f158fe301b1b0bd71df47230c8914671717
Julia Amadio <71294182+JKAmadio@users.noreply.github.com>
2022-12-18 11:03:43 -0300

    part04 - store api - basic setup

--- 04-store-api/app.js
+++ 04-store-api/app.js
@@ -1,1 +1,30 @@
-console.log('04 Store API')
+const express = require("express");
+const connectDB = require("./db/connect");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const notFound = require("./middleware/not-found");
+const products = require("./routes/products");
+require("dotenv").config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send('<h1>Store API</h1><a href="api/v1/products/">products</a>');
+});
+
+app.use("/api/v1/products", products);
+
+app.use(errorHandlerMiddleware);
+app.use(notFound);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGODB);
+    app.listen(port, console.log(`Server listening to port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ 04-store-api/controllers/products.js
@@ -0,0 +1,5 @@
+const getAllProducts = (req, res) => {
+  res.send("all products constrollers");
+};
+
+module.exports = { getAllProducts };
