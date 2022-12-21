c3d703370a764494e33dc88c68816d14e1190dc5
Julia Amadio <71294182+JKAmadio@users.noreply.github.com>
2022-12-21 16:37:03 -0300

    part04 - store api - update product schema

--- 04-store-api/models/product.js
+++ 04-store-api/models/product.js
@@ -1,9 +1,32 @@
 const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema({
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
   name: {
     type: String,
-    required: true,
+    required: [true, "product name must be provided"],
+  },
+  price: {
+    type: Number,
+    required: [true, "product price must be provided"],
+  },
+  company: {
+    type: String,
+    enum: {
+      values: ["ikea", "liddy", "caressa", "marcos"], // we enumarate some specific values that the user can use
+      message: "{VALUE} is not supported", // we access the user input by using {PROP_NAME} and send a message about the error
+    },
   },
 });
 
