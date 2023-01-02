3d4125959301a8d2f9f04811f799320eda4ee868
Julia Amadio <71294182+JKAmadio@users.noreply.github.com>
2023-01-02 20:33:01 -0300

    part06 - jobs api - hashing the password

--- 06-jobs-app/controllers/auth.js
+++ 06-jobs-app/controllers/auth.js
@@ -1,5 +1,21 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const bcrypt = require("bcryptjs");
+
 const register = async (req, res) => {
-  res.send("register user");
+  const { name, email, password } = req.body;
+
+  // we call the 'genSalt' method to generate the hashing base
+  // the '10' is the default value, bigger the number, more secure it is, but more proccessing power required
+  const salt = await bcrypt.genSalt(10);
+
+  // we call the 'hash' method to execute the hashing to the password, using the salt as base
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const tempUser = { name, email, password: hashedPassword };
+
+  const user = await User.create(tempUser);
+
+  res.status(StatusCodes.CREATED).json(user);
 };
 
 const login = async (req, res) => {
