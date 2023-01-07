a78650edab04fd222a215fb3470bba58a06affdf
Julia Amadio <71294182+JKAmadio@users.noreply.github.com>
2023-01-07 17:12:00 -0300

    part06 - jobs api - fixing required property spelling

--- 06-jobs-app/models/Job.js
+++ 06-jobs-app/models/Job.js
@@ -24,7 +24,7 @@ const JobSchema = new mongoose.Schema(
       // creating a link between the User model and the Job model
       type: mongoose.Types.ObjectId,
       ref: "User",
-      require: [true, "Please provide User"],
+      required: [true, "Please provide User"],
     },
   },
   { timestamps: true }
--- 06-jobs-app/models/User.js
+++ 06-jobs-app/models/User.js
@@ -5,13 +5,13 @@ const jwt = require("jsonwebtoken");
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    require: [true, "Please provide name"],
+    required: [true, "Please provide name"],
     minlength: 3,
     maxlength: 50,
   },
   email: {
     type: String,
-    require: [true, "Please provide email"],
+    required: [true, "Please provide email"],
     match: [
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       "Please provide a valid email",
@@ -20,7 +20,7 @@ const UserSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    require: [true, "Please provide password"],
+    required: [true, "Please provide password"],
     minlength: 6,
   },
 });
