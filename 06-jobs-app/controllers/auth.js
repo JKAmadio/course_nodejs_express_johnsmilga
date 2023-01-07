1ee416508c07d5feebd42851a75da6f7cfb2f121
Julia Amadio <71294182+JKAmadio@users.noreply.github.com>
2023-01-06 21:43:44 -0300

    part06 - jobs api - createJWT instance method

--- 06-jobs-app/controllers/auth.js
+++ 06-jobs-app/controllers/auth.js
@@ -1,16 +1,12 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   const user = await User.create(req.body);
 
-  const token = jwt.sign(
-    { userId: user._id, name: user.name },
-    process.env.JWT_SECRET,
-    { expiresIn: "30d" }
-  );
-
+  // we evolke the createJWT instance method
+  // see: 'models/User.js'
+  const token = user.createJWT();
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
--- 06-jobs-app/models/User.js
+++ 06-jobs-app/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -26,6 +27,7 @@ const UserSchema = new mongoose.Schema({
 
 // we set a Mongoose Middleware to execute functions before save on the database
 // we can access the model Document through the 'this' keyword
+// infos: https://mongoosejs.com/docs/middleware.html#pre
 UserSchema.pre("save", async function () {
   // we call the 'genSalt' method to generate the hashing base
   // the '10' is the default value, bigger the number, more secure it is, but more proccessing power required
@@ -35,5 +37,16 @@ UserSchema.pre("save", async function () {
   // we replace the password that is coming through the "create" method with the hashed one
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+// we create a method that can be called in any Document instance
+// in this case, we are calling after creating the user Document (see 'controllers/auth.js')
+// infos: https://mongoosejs.com/docs/middleware.html#types-of-middleware
+UserSchema.methods.createJWT = function () {
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_LIFETIME }
+  );
+};
 
 module.exports = mongoose.model("User", UserSchema);
